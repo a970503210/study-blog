7be9dec01ff828dc3b55be911e75ba35f9e9d7c7
QYP_2974 <970503210@qq.com>
2021-05-28 10:33:25 +0800

    创建项目

---
+++ src/test/java/cn/purity/studyblog/StudyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.purity.studyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
